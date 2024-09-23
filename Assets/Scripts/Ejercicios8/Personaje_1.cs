81e2861396a73d4345726942c008eb5962697a20
estef157 <estefaniaizarra8@gmail.com>
2024-09-23 14:23:54 +0200

    repaso 23/09

---
+++ Assets/Scripts/Ejercicios8/Personaje_1.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personaje_1 : MonoBehaviour
+{
+    string nombre = "Paulina";
+    float vida = 100;
+    float experiencia = 100;
+    
+
+    private void Start()
+    {
+        
+    }
+
+    void CalcularNivel ()
+    {
+
+    }
+}
